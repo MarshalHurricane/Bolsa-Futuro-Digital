4cf9942ab47879ae47df09ea8dc73b6a5217d0d5
Gabriel Chaves <gabrielccavalcante@gmail.com>
2025-08-28 14:29:05 -0300

    Finalizando exericicio15.js

--- pratica/aula8/exercicio15.js
+++ pratica/aula8/exercicio15.js
@@ -1,14 +1,10 @@
-<<<<<<< HEAD
-/*
-Você tem um array de objetos representando vendas de uma vendasLoja. Cada objeto possui produto, quantidade e precoUnitario.Cada
+/* Você tem um array de objetos representando vendas de uma vendasLoja. Cada objeto possui produto, quantidade e precoUnitario.Cada
     Crie uma função que calcula o total de uma venda (quantidade * precoUnitario). 
     Crie uma função que percorre o array usando forEach e imprime:
     * "PRODUTO vendeu TOTAL reais" para cada venda
     * Ao final, imprime "TOtal vendido na loja: TOTAL_GERAL reais"
 */
 
-=======
->>>>>>> ab3a6e7e1ef0496662e20c867ad0913788562145
 const vendasLoja = [
     {produto: "nutela140g", quantidade: 30, precoUnitario: 12.99},
     {produto: "ouroBranco1kg", quantidade: 45, precoUnitario: 54.99},
@@ -17,36 +13,23 @@ const vendasLoja = [
     {produto: "finiTubes180g", quantidade: 88, precoUnitario: 8.99}
 ]
 
-<<<<<<< HEAD
-
-
-function totalVendas(vendas) {
-     return totalProduto = vendas.quantidade * vendas.precoUnitario;
+function totalProduto(venda) {
+     return venda.quantidade * venda.precoUnitario;
     
 }
     
-function relatorioVendas() {
-    
-    let totalGeral
-    vendasLoja.forEach(function(elemento) {
-    console.log(`${vendasLoja.produto} vendeu ${totalVendas}`);
+function relatorioVendas(vendas) {
+    let totalGeral = 0
+
+    vendasLoja.forEach(function(venda) {
+        const totalVenda = totalProduto(venda);
+        totalGeral += totalVenda;
+        console.log(`${venda.produto} vendeu ${totalVenda.toFixed(2)}`);
     })
 
-    let totalGeral = totalProduto * vendasLoja.length;
 
-    console.log(`Total vendido na loja: ${totalGeral}`);
+    console.log(`Total vendido na loja: R$${totalGeral.toFixed(2)}`);
 
 }
 
-vendasLoja.forEach (totalVendas)
-relatorioVendas()
-=======
-function totalVendas() {
-    for (let i = 0; i < vendasLoja.length; i++) {
-        console.log(vendasLoja[i].quantidade * vendasLoja[i].precoUnitario)
-    }
-    
-}
-
-console.log(totalVendas(vendasLoja))
->>>>>>> ab3a6e7e1ef0496662e20c867ad0913788562145
+console.log(relatorioVendas(vendasLoja))
