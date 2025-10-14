5f8625b662b6fa8e0e600d23c7b98d1e591bbc44
GabrielChaves <gabrielccavalcante@gmail.com>
2025-10-14 10:07:30 -0300

    (feat) Continuando exercício de API.

--- pratica/aula19-20/src/router/app.ts
+++ pratica/aula19-20/src/router/app.ts
@@ -2,19 +2,19 @@
 import express , { Request , Response } from "express";
 import { Express } from "express";
 import { Produto } from "./Produto";
+import produtoRouter from
 
 const app = express();
 app.use(express.json());
 const PORT = 3000;
 
+app.use("/api", produtorouter)
 
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost: ${PORT}`);
 });
 
-app.post("/Produto", (req : Request, rep : Response) => {
-    
-})
+
 
 
--- pratica/aula19-20/src/router/produto.router.ts
+++ pratica/aula19-20/src/router/produto.router.ts
@@ -4,7 +4,7 @@ import express , {Request , Response} from "express";
 
 const router = Router();
 
-const Estoque = [
+const estoque: Produto[] = [
     new Produto(13123, "rtx4060", 2000),
     new Produto(12372, "rtx4070", 3800),
     new Produto(14123, "rtx4080", 10500),
@@ -13,15 +13,14 @@ const Estoque = [
 
 
 router.get("/Estoque", (req : Request, res : Response) => {
-    res.send("")
+    res.status(200).json(estoque);
 })
 
-router.get("Usuario")
+router.post("/inserirProduto", (req , res) => {
+    const { id, nome, preco } = req.body
+    const produto = new Produto(id, nome, preco)
 
+    res.send("Produto cadastrado com sucesso.")
+})
 
-/*
-const produto1 = new Produto(13123, "rtx4060");
-const produto2 = new Produto(12372, "rtx4070");
-const produto3 = new Produto(14123, "rtx4080");
-const produto4 = new Produto(19832, "rtx4090");
-*/
+export default router;
