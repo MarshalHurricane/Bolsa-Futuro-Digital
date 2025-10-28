e92bd01cf373734e24b76f242092fd69df6e9fa2
GabrielChaves <gabrielccavalcante@gmail.com>
2025-10-28 10:17:33 -0300

    (new)ExercicioTADS temporário.

---
+++ pratica/loopwhile.c
@@ -0,0 +1,15 @@
+#include <stdio>;
+
+    int main () {
+        float A;
+        float B;
+        float C;
+        printf("Digite o valor do primeiro numero."\n);
+        scanf("%f", &A);
+        printf("Digite o valor do segundo numero."\n);
+        scanf("%f", &B);
+        printf("Digite o valor do terceiro numero."\n);
+        scanf("%f", &C);
+        while ()
+
+    }
